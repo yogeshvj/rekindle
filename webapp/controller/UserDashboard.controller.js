89b4e889879a8b2996cf4cbd3391ad7e98e55f48
Priyanka Gupta <priyanka.gupta04@sap.com>
2020-07-03 17:39:08 +0530

    first commit

---
+++ webapp/controller/UserDashboard.controller.js
@@ -0,0 +1,189 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./Dialog1",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(BaseController, MessageBox, Dialog1, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.reKindle.controller.UserDashboard", {
+		handleRouteMatched: function(oEvent) {
+			var sAppId = "App5edf12ba5db36c1e792a29cc";
+
+			var oParams = {};
+
+			if (oEvent.mParameters.data.context) {
+				this.sContext = oEvent.mParameters.data.context;
+
+			} else {
+				if (this.getOwnerComponent().getComponentData()) {
+					var patternConvert = function(oParam) {
+						if (Object.keys(oParam).length !== 0) {
+							for (var prop in oParam) {
+								if (prop !== "sourcePrototype" && prop.includes("Set")) {
+									return prop + "(" + oParam[prop][0] + ")";
+								}
+							}
+						}
+					};
+
+					this.sContext = patternConvert(this.getOwnerComponent().getComponentData().startupParameters);
+
+				}
+			}
+
+			var oPath;
+
+			if (this.sContext) {
+				oPath = {
+					path: "/" + this.sContext,
+					parameters: oParams
+				};
+				this.getView().bindObject(oPath);
+			}
+
+		},
+		_onIconPress: function(oEvent) {
+
+			var sDialogName = "Dialog1";
+			this.mDialogs = this.mDialogs || {};
+			var oDialog = this.mDialogs[sDialogName];
+			if (!oDialog) {
+				oDialog = new Dialog1(this.getView());
+				this.mDialogs[sDialogName] = oDialog;
+
+				// For navigation.
+				oDialog.setRouter(this.oRouter);
+			}
+
+			var context = oEvent.getSource().getBindingContext();
+			oDialog._oControl.setBindingContext(context);
+
+			oDialog.open();
+
+		},
+		_onIconPress1: function(oEvent) {
+
+			var sDialogName = "Dialog1";
+			this.mDialogs = this.mDialogs || {};
+			var oDialog = this.mDialogs[sDialogName];
+			if (!oDialog) {
+				oDialog = new Dialog1(this.getView());
+				this.mDialogs[sDialogName] = oDialog;
+
+				// For navigation.
+				oDialog.setRouter(this.oRouter);
+			}
+
+			var context = oEvent.getSource().getBindingContext();
+			oDialog._oControl.setBindingContext(context);
+
+			oDialog.open();
+
+		},
+		_onIconPress2: function(oEvent) {
+
+			var sDialogName = "Dialog1";
+			this.mDialogs = this.mDialogs || {};
+			var oDialog = this.mDialogs[sDialogName];
+			if (!oDialog) {
+				oDialog = new Dialog1(this.getView());
+				this.mDialogs[sDialogName] = oDialog;
+
+				// For navigation.
+				oDialog.setRouter(this.oRouter);
+			}
+
+			var context = oEvent.getSource().getBindingContext();
+			oDialog._oControl.setBindingContext(context);
+
+			oDialog.open();
+
+		},
+		_onIconPress3: function(oEvent) {
+
+			var sDialogName = "Dialog1";
+			this.mDialogs = this.mDialogs || {};
+			var oDialog = this.mDialogs[sDialogName];
+			if (!oDialog) {
+				oDialog = new Dialog1(this.getView());
+				this.mDialogs[sDialogName] = oDialog;
+
+				// For navigation.
+				oDialog.setRouter(this.oRouter);
+			}
+
+			var context = oEvent.getSource().getBindingContext();
+			oDialog._oControl.setBindingContext(context);
+
+			oDialog.open();
+
+		},
+		_onIconPress4: function(oEvent) {
+
+			var sDialogName = "Dialog1";
+			this.mDialogs = this.mDialogs || {};
+			var oDialog = this.mDialogs[sDialogName];
+			if (!oDialog) {
+				oDialog = new Dialog1(this.getView());
+				this.mDialogs[sDialogName] = oDialog;
+
+				// For navigation.
+				oDialog.setRouter(this.oRouter);
+			}
+
+			var context = oEvent.getSource().getBindingContext();
+			oDialog._oControl.setBindingContext(context);
+
+			oDialog.open();
+
+		},
+		_onIconPress5: function(oEvent) {
+
+			var sDialogName = "Dialog1";
+			this.mDialogs = this.mDialogs || {};
+			var oDialog = this.mDialogs[sDialogName];
+			if (!oDialog) {
+				oDialog = new Dialog1(this.getView());
+				this.mDialogs[sDialogName] = oDialog;
+
+				// For navigation.
+				oDialog.setRouter(this.oRouter);
+			}
+
+			var context = oEvent.getSource().getBindingContext();
+			oDialog._oControl.setBindingContext(context);
+
+			oDialog.open();
+
+		},
+		_onButtonPress: function() {
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+			var oQueryParams = this.getQueryParameters(window.location);
+
+			if (sPreviousHash !== undefined || oQueryParams.navBackToLaunchpad) {
+				window.history.go(-1);
+			} else {
+				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+				oRouter.navTo("default", true);
+			}
+
+		},
+		getQueryParameters: function(oLocation) {
+			var oQuery = {};
+			var aParams = oLocation.search.substring(1).split("&");
+			for (var i = 0; i < aParams.length; i++) {
+				var aPair = aParams[i].split("=");
+				oQuery[aPair[0]] = decodeURIComponent(aPair[1]);
+			}
+			return oQuery;
+
+		},
+		onInit: function() {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("UserDashboard").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+
+		}
+	});
+}, /* bExport= */ true);
